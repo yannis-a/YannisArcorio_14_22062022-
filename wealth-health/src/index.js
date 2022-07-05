221763f5897b0b97c8b1d102f60a2d186ee12c5c
Yannis <arcorio.y@gmail.com>
2022-07-05 23:48:16 +0200

    uses react persist

--- wealth-health/src/index.js
+++ wealth-health/src/index.js
@@ -4,12 +4,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
-
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
